02461725619fe8610d03fa14cdea41d4425e08fb
MarijaaStupar <marija.stupar03@gmail.com>
2026-08-29 21:08:38 +0200

    Ubacene tabele i pocetni podaci i kreirana konekcija sa bazom

---
+++ src/db/seed.ts
@@ -0,0 +1,61 @@
+import "dotenv/config";
+import { db } from "./index";
+import { zaposleniTable } from "./schema";
+
+async function main() {
+  const today = new Date().toISOString().slice(0, 10);
+
+  try {
+    await db.transaction(async (tx) => {
+      await tx
+      .insert(zaposleniTable)
+      .values({
+        ime: "Ana",
+        prezime: "Menadzer",
+        pozicija: "HR Menadžer",
+        plata: "8000.00",
+        datum_dolaska: today,
+        datum_odlaska: null,
+        username: "manager",
+        password: "manager123",
+        uloga: "MENADZER U HR-u",
+      })
+
+      await tx.insert(zaposleniTable).values({
+      ime: "Marko",
+      prezime: "HR",
+      pozicija: "HR Zaposleni",
+      plata: "5000.00",
+      datum_dolaska: today,
+      datum_odlaska: null,
+      username: "hr",
+      password: "hr123",
+      uloga: "ZAPOSLENI U HR-u",
+      });
+
+      await tx
+      .insert(zaposleniTable)
+      .values({
+        ime: "Petar",
+        prezime: "Petrovic",
+        pozicija: "Developer",
+        plata: "4500.00",
+        datum_dolaska: today,
+        datum_odlaska: null,
+        username: "petar",
+        password: "petar123",
+        uloga: "ZAPOSLENI",
+      })
+    })
+  } catch (e) {
+    console.log(e)
+  }
+  console.log("Seed uspešno završen!!!")
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
